f662b0502c659a3baab3e5b57984abd21b9be893
Aidancartney24 <824776@mystma.org>
2021-09-21 14:32:57 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+print(topFiveMovies("up", "any star wars", "spiderman two", "infinity war", "matrix"))
